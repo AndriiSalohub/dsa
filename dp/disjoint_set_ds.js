b9d3adf42ce8fc2eb3646d96247ec086defeea29
Andrii Salohub <andrejsalogub3@gmail.com>
2024-11-15 11:16:42 +0200

    Add linked list implementation for disjoint-set data structure

--- dp/disjoint_set_ds.js
+++ dp/disjoint_set_ds.js
@@ -24,3 +24,52 @@ const union = (dsu, n, a, b) => {
     }
   }
 };
+
+// Реалізація за допомогоб пов'язаних списків
+class Item {
+  constructor(id) {
+    this.id = id;
+    this.head = this;
+    this.tail = this;
+    this.next = null;
+
+    this.count = 1;
+  }
+}
+
+const initDSULL = (n) => {
+  const dsu = [];
+
+  for (let i = 0; i < n; i++) {
+    dsu.push(new Item(i));
+  }
+
+  return dsu;
+};
+
+const findLL = (a) => {
+  return a.head.id;
+};
+
+const unionList = (a, b) => {
+  a = a.head;
+  b = b.head;
+
+  if (a === b) {
+    return;
+  }
+
+  if (b.count > a.count) {
+    [a, b] = [b, a];
+  }
+
+  a.tail.next = b;
+  a.tail = b.tail;
+
+  a.count += b.count;
+
+  while (b) {
+    b.head = a;
+    b = b.next;
+  }
+};
